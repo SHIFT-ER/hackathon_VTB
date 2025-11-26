69ea4da456ba76476052e415f8109ee5e13e1072
NightSHIFTER <119579342+SHIFT-ER@users.noreply.github.com>
2025-11-26 20:40:41 +0300

    gg

--- src/migrate_schema.sh
+++ src/migrate_schema.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+# Скрипт для миграции всей схемы (schema)
 
 migrate_schema() {
-    local source_db=$1
-    local target_db=$2
-    local schema_name=$3
+    local src_host="$1"
+    local src_port="$2"
+    local src_db="$3"
+    local src_user="$4"
+    local tgt_host="$5"
+    local tgt_port="$6"
+    local tgt_db="$7"
+    local tgt_user="$8"
+    local schema_name="$9"
 
-    echo "Migrating schema $schema_name from $source_db to $target_db"
-    
-    # Используем pg_dump для создания дампа схемы
-    pg_dump -h $source_db -U admin -n $schema_name $source_db | pg_restore -h $target_db -U admin -d $target_db
+    echo "Migrating schema $schema_name from $src_db to $tgt_db"
+
+    pg_dump -h "$src_host" -p "$src_port" -U "$src_user" -n "$schema_name" "$src_db" \
+      | pg_restore -h "$tgt_host" -p "$tgt_port" -U "$tgt_user" -d "$tgt_db"
 }
-
-log_message "Started migrating table $table_name"
--- src/migrate_table.sh
+++ src/migrate_table.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+# Скрипт для миграции одной таблицы
 
 migrate_table() {
-    local source_db=$1
-    local target_db=$2
-    local table_name=$3
+    local src_host="$1"
+    local src_port="$2"
+    local src_db="$3"
+    local src_user="$4"
+    local tgt_host="$5"
+    local tgt_port="$6"
+    local tgt_db="$7"
+    local tgt_user="$8"
+    local table_name="$9"
 
-    echo "Migrating table $table_name from $source_db to $target_db"
-    
-    # Используем pg_dump для создания дампа таблицы
-    pg_dump -h $source_db -U admin -t $table_name $source_db | pg_restore -h $target_db -U admin -d $target_db
+    echo "Migrating table $table_name from $src_db to $tgt_db"
+
+    pg_dump -h "$src_host" -p "$src_port" -U "$src_user" -t "$table_name" "$src_db" \
+      | pg_restore -h "$tgt_host" -p "$tgt_port" -U "$tgt_user" -d "$tgt_db"
 }
-
-log_message "Started migrating table $table_name"
